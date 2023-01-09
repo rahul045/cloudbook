76b82b7591185d9e89a0086e03f1b7fb63f6640f
Rahul <rahulkalra045@gmail.com>
2023-01-09 13:38:18 +0530

    completd

--- src/Components/Noteitem.js
+++ src/Components/Noteitem.js
@@ -1,15 +1,28 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
+import NoteContext from '../Context/notes/NoteContext';
 
 const Noteitem = (props) => {
-    const { note } = props;
+    const { note, updateNote } = props;
+    const context = useContext(NoteContext);
+    const { deleteNote } = context;
+
     return (
 
         <div className="col-md-3">
             <div className="card my-3">
                 <div className="card-body">
                     <h5 className="card-title"> {note.title}</h5>
+                    <h5 className="card-title"> {note.tag}</h5>
+
                     <p className="card-text">{note.description}</p>
+                    <i className="fa-solid fa-pen-to-square mx-2" onClick={() => {
+                        updateNote(note);
+
+                    }}></i>
+                    <i className="fa-solid fa-trash-can mx-2" onClick={() => {
+                        deleteNote(note._id);
+                    }}></i>
                 </div>
             </div>
         </div>
